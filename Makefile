89db6450753ccaa97b62e4e902b75d9e62bef0ea
Luca <yangshaoxiong5545@gmail.com>
2025-07-07 18:25:22 +0800

    feat: Trigger 签名校验
    wip

--- Makefile
+++ Makefile
@@ -6,4 +6,6 @@ restart:
 	@-echo "Copying icon..."
 	@-cp nodes/JoggAiNode/joggai.png dist/nodes/JoggAiNode/joggai.png
 	@-cp nodes/JoggAiWebhookTrigger/joggai.png dist/nodes/JoggAiWebhookTrigger/joggai.png
+	# 在启动 n8n 时加上，否则拿不到原始 http request payload
+	export N8N_PAYLOAD_DIGEST=true
 	npx n8n
--- nodes/JoggAiWebhookTrigger/JoggAiWebhookTrigger.node.ts
+++ nodes/JoggAiWebhookTrigger/JoggAiWebhookTrigger.node.ts
@@ -1,4 +1,10 @@
-import { INodeType, INodeTypeDescription, NodeConnectionType } from 'n8n-workflow';
+import crypto from 'crypto';
+import {
+	INodeType,
+	INodeTypeDescription,
+	NodeConnectionType,
+	NodeOperationError,
+} from 'n8n-workflow';
 import { IWebhookFunctions } from 'n8n-workflow/dist/Interfaces';
 
 import { CREDENTIALS_API_NAME } from '../../const/joggAiNode';
@@ -31,13 +37,53 @@ export class JoggAiWebhookTrigger implements INodeType {
 				path: 'jogg',
 			},
 		],
-		properties: [],
+		properties: [
+			{
+				displayName: 'Webhook Secret',
+				name: 'webhookSecret',
+				// eslint-disable-next-line n8n-nodes-base/node-param-type-options-password-missing
+				type: 'string',
+				default: '',
+			},
+		],
 	};
 
 	async webhook(this: IWebhookFunctions): Promise<any> {
+		const webhookSecret = this.getNodeParameter('webhookSecret', '') as string;
+		const headerData = this.getHeaderData();
 		const body = this.getBodyData();
+
+		// Get signature
+		let signature = '';
+		const headerKeys = Object.keys(headerData);
+		const signatureHeaderKey = headerKeys.find(
+			(key) => key.toLowerCase() === 'x-webhook-signature',
+		);
+		if (signatureHeaderKey) {
+			signature = headerData[signatureHeaderKey] as string;
+		}
+
+		if (webhookSecret && signature) {
+			this.logger.info('If the secret is set and the signature is not empty, verify the signature');
+			// Calculate signature
+			const mac = crypto.createHmac('sha256', webhookSecret);
+			mac.update(JSON.stringify(body));
+			const calculatedSignature = mac.digest('hex');
+
+			// Compare signature
+			if (calculatedSignature !== signature) {
+				throw new NodeOperationError(this.getNode(), 'JoggAI Webhook signature validation failed');
+			}
+		}
+
 		return {
-			workflowData: [[{ json: body }]],
+			workflowData: [
+				[
+					{
+						json: body,
+					},
+				],
+			],
 		};
 	}
 }
